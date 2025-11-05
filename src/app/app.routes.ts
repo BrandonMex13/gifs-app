6498513fded998d48254535a9e81481121eb55eb
Brandon Montoya <brandonmex13@gmail.com>
2025-11-04 21:21:26 -0700

    Se sube los primeros cambios del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page').then( c => c.DashboardPage),
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () => import('./gifs/pages/trending-page/trending-page').then( c => c.TrendingPage)
+      },
+      {
+        path: 'search',
+        loadComponent: () => import('./gifs/pages/search-page/search-page').then( c => c.SearchPage)
+      },
+      {
+        path: '**',
+        redirectTo: 'trending'
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
---
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOption {
+  icon     : string;
+  label    : string;
+  sublabel : string;
+  route    : string;
+}
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './gifs-side-menu-options.html',
+  styleUrl: './gifs-side-menu-options.css',
+})
+export class GifsSideMenuOptions {
+  menuOptions : MenuOption[] = [
+    {
+      icon     :  'fa-solid fa-chart-line',
+      label    :  'Trending',
+      sublabel :  'Gifs populares',
+      route    :  '/dashboard/trending',
+    },
+    {
+      icon     :  'fa-solid fa-magnifying-glass',
+      label    :  'Buscador',
+      sublabel :  'Buscar Gifs',
+      route    :  '/dashboard/search',
+    }
+  ]
+}
